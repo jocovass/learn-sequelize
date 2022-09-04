ab3a9b02071e619d6ba4d2b357dc710059a26cec
JozsefVass <joco.udv@gmail.com>
2022-09-04 12:28:25 +0100

    Create models

--- index.js
+++ index.js
@@ -1,11 +1,51 @@
-const { Sequelize } = require("sequelize");
+const { Sequelize, DataTypes } = require("sequelize");
 
+// connect to the databes two different method
 // const seqInstance = new Sequelize("postgresql://localhost:5432/joco");
 const seqInstance = new Sequelize("joco", "", "", {
   host: "localhost",
   dialect: "postgres",
 });
 
+// create a new table
+const User = seqInstance.define(
+  "user",
+  {
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+    },
+    age: {
+      type: DataTypes.INTEGER,
+      defaultValue: 18,
+    },
+  },
+  {
+    freezeTableName: true,
+  }
+);
+
+// Syncronize the above defined options with the database
+// sync() => creates a table if it doesn't exist
+// sync({force: true}) => recreates the table
+// sync({alter: true}) => perform only necessery changes
+User.sync({ alter: true })
+  .then((data) => {
+    console.log(`User sync successful ${data}`);
+  })
+  .catch((error) => {
+    console.log(`User sync failed ${error}`);
+  });
+
+// we can call .sync() on the sequelize instace to sync multiple models also to drop tables
+// THESE ARE NOT RECOMENDED because they are destructive operations
+// seqInstance.sync().then(() => console.log('same'))
+// seqInstance.drop({ match: 'some reqex here '})
+
+// check if the connection was successful
 seqInstance
   .authenticate()
   .then(() => console.log("Connection successful"))
